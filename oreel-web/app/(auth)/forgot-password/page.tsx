0ede9e30756a216ecfc08419c0d4f76ec627b6fb
Lydia Oreoluwa <oreoluwabambams@gmail.com>
2026-03-14 21:59:45 +0100

    feat: added a comment to forgot-password/page.tsx

--- oreel-web/app/(auth)/forgot-password/page.tsx
+++ oreel-web/app/(auth)/forgot-password/page.tsx
@@ -1,3 +1,3 @@
-export default function ForgotPasswordPage() {
+export default function ForgotPasswordPage() { // This page is not implemented yet, but we want to have it in place for the future.
   return null
 }
